dcc7db018632ce05c604974aff689044c1f07c1c
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-09-26 09:08:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    let rain = false
+    if (rain == false) {
+        basic.showString("raincoat")
+    } else {
+        basic.showString("shorts")
+    }
+})
+let _var = false
+basic.forever(function () {
+	
+})
